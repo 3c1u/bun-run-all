3bda03006b15866cd8bc3d2a7cbef35814d57a86
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-09-13 02:12:08 +0900

    feat: npm-run-all

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
-export const main = () => {
-  // TODO: implement
+import { Command } from 'commander'
+import { runParallelTasks } from './run-p'
+
+const program = new Command()
+
+program
+  .usage('[options] <tasks...>')
+  .description('Runs tasks in parallel or sequentially.')
+  .arguments('<tasks...>')
+  .option('-p, --parallel', 'Run tasks in parallel', true)
+  .option('-s, --sequential', 'Run tasks sequentially')
+  .option('-c, --continue-on-error', 'Continue on error')
+  .option('-j, --max-parallel <number>', 'Max concurrency', '4')
+  .option(
+    '--aggregate-output',
+    'Aggregates the output of all tasks by throttling',
+  )
+  .option('-l, --print-label', 'Prepends the task name to its output')
+  .option('-n, --print-name', 'Prints the task name before running it')
+  .option('-r, --race', 'Kills all tasks if one finishes successfully')
+  .option('-s, --silent', 'Suppresses all output')
+
+export const main = async () => {
+  const parsedArguments = program.parse(process.argv)
+  const options = program.opts()
+  const tasks = parsedArguments.args
+
+  if (options.parallel && options.sequential) {
+    throw new Error('Cannot run tasks in parallel and sequentially.')
+  }
+
+  await runParallelTasks(tasks, {
+    continueOnError: Boolean(options.continueOnError),
+    maxConcurrency: options.parallel ? Number(options.maxParallel) || 4 : 1,
+  })
+}
+
+if (import.meta.path === Bun.main) {
+  await main()
 }
